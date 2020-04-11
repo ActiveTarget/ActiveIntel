c030f4e6868bcfb9a4b638a7b49a55070ae4f142
Adam Dalton <adalton@ihmc.us>
2020-04-11 02:13:51 -0400

    working with autoloaders

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,13 @@ module ActiveIntel
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    overrides = "#{Rails.root}/app/overrides"
+    Rails.autoloaders.main.ignore(overrides)
+    config.to_prepare do
+      Dir.glob("#{overrides}/**/*_override.rb").each do |override|
+        load override
+      end
+    end
+
   end
 end
